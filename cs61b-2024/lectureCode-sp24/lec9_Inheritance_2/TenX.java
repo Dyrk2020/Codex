46866c027020a2a5eabcb5f323d72953799430ef
Dyrk2020 <Dyrk2020@users.noreply.github.com>
2025-03-30 15:30:00 +0800

    Add CS61B lecture code and site mirror

---
+++ cs61b-2024/lectureCode-sp24/lec9_Inheritance_2/TenX.java
@@ -0,0 +1,11 @@
+package Lec9_Inheritance_2;
+
+/**
+ * Created by hug on 2/6/2017.
+ */
+public class TenX implements IntUnaryFunction {
+    /** Returns ten times the argument. */
+    public int apply(int x) {
+        return 10 * x;
+    }
+}
